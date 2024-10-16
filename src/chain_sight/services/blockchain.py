e862a99584a3ec4eeeb43249e9798a86835ea0e9
qf3l3k <qf3l3k@gmail.com>
2024-10-16 15:44:40 +0200

    Debug logging added to delegators fetching.

--- src/chain_sight/services/blockchain.py
+++ src/chain_sight/services/blockchain.py
@@ -42,8 +42,8 @@ def fetch_validators(chain_config):
 
 
 def fetch_and_store_delegators(validator_addr, chain_config):
-    logger.debug(f'Fetching delegators data from {chain_config.api_endpoint}.')
     delegations_endpoint = f"{chain_config.api_endpoint}/cosmos/staking/v1beta1/validators/{validator_addr}/delegations"
+    logger.debug(f'Fetching delegators data from {delegations_endpoint}.')
     active_delegator_addresses = []  # Initialize an empty list to collect active delegator addresses
 
     next_key = None  # Initialize the pagination key
