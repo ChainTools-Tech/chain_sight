ffca5aa5b951af21edb33b42277eaaa9297febab
qf3l3k <qf3l3k@gmail.com>
2024-10-16 11:04:55 +0200

    Folder structure updated.

--- src/chain_sight/__main__.py
+++ src/chain_sight/__main__.py
@@ -1,8 +1,8 @@
 import logging
 
-from common.cli import parse_args
-from common.logger import get_log_level, setup_logging
-from services.database_config import initialize_database
+from chain_sight.common.cli import parse_args
+from chain_sight.common.logger import get_log_level, setup_logging
+from chain_sight.services.database_config import initialize_database
 
 
 
